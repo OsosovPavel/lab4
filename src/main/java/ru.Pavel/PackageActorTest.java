8ef1ebddd3c807acd20f2448de2219aeb908d597
OsosovPavel <Ososowpp@yandex.ru>
2020-12-25 16:54:47 +0300

    Scripted auto-commit on change (2020-12-25 16:54:45) by gitwatch.sh

--- src/main/java/ru.Pavel/PackageActorTest.java
+++ src/main/java/ru.Pavel/PackageActorTest.java
@@ -1,2 +1,23 @@
-package ru.Pavel;public class PackageActorTest {
+package ru.Pavel;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorSelection;
+import akka.actor.ActorSystem;
+import akka.japi.pf.ReceiveBuilder;
+
+
+
+public class PackageActorTest extends AbstractActor {
+ private  ActorSelection testPerformerRouter = getContext().actorSelection("/user/testPerformerActor");
+
+ public Receive createReveive() {
+     return ReceiveBuilder.create()
+             .match(PackageMessageTest.class, m -> {
+                 for (Test test: m.getTests()) {
+                     testPerformerRouter
+                             .tell(new TestMessage(m.getPackageId(), m.getJsScript(), m.getFunctionName(), test), self());
+                 }
+             })
+             .build()
+ }
 }
